38bfd9d460015ca01cdd3e7c252455f46aece654
unknown <rajebul.alam17@gmail.com>
2022-05-07 09:54:11 +0600

    Project Usert and Password Secured

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-const uri = "mongodb+srv://khokan:G0cyMeBUbvZU5HD8@cluster0.0mqvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0mqvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // Main Function
