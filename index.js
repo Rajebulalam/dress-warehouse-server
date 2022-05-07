bed118c0b96a1b23a714c388acb9bd06e04db8a2
unknown <rajebul.alam17@gmail.com>
2022-05-07 09:27:01 +0600

    Database Connected With Local Server

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+const uri = "mongodb+srv://khokan:G0cyMeBUbvZU5HD8@cluster0.0mqvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('db connected');
+    client.close();
+});
+
 
 // Server Home Page
 app.get('/', async (req, res) => {
@@ -16,3 +28,6 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log('Server is running', port);
 })
+
+// User : khokan
+// Pass : G0cyMeBUbvZU5HD8
