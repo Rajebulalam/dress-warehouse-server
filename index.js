6f09a1589d38adeea9dff8d47ff2a8c48b894f41
unknown <rajebul.alam17@gmail.com>
2022-05-08 21:06:26 +0600

    Product Delete from DB

--- index.js
+++ index.js
@@ -51,13 +51,20 @@ async function run() {
             res.send(result);
         });
 
+        // Delete Item
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
     } finally {
 
     }
 }
 
 run().catch(console.dir);
-
 
 // Server Home Page
 app.get('/', async (req, res) => {
