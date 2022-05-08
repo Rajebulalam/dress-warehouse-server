179a29e7551650ee2ef2a67d24b513d57a37a521
unknown <rajebul.alam17@gmail.com>
2022-05-08 16:45:28 +0600

    Quantity Reducing Updated

--- index.js
+++ index.js
@@ -36,6 +36,21 @@ async function run() {
             res.send(result);
         });
 
+        // Update Quantity by Id
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
     } finally {
 
     }
