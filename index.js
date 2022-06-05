02d94f0d185cbccf382c34bf680eac508717448a
unknown <rajebul.alam17@gmail.com>
2022-06-05 09:58:32 +0600

    Code Modified

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
             res.send(result);
         });
 
-        // Delete Item
+        // Delete Item from DB
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
